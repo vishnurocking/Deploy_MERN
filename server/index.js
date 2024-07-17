4418e540bf40819594f44976fab8c871c1a526c5
Vishnu S <70127764+vishnurocking@users.noreply.github.com>
2024-07-17 13:18:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://vishnu:vishnu123@cluster0.nj8sfsz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://vishnu:vishnu123@cluster0.nj8sfsz.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
 
 
 app.get("/", (req, res) => {
